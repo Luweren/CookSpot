dce3a9ca11734e1230de166376f71fa9736b39c1
Luweren <luwerenmade@gmail.com>
2021-05-28 13:08:19 +0200

    created models and migrated
    First Model designs for the user/recipe/ingredient/meets, further updates upon testing with html

--- cookproject/CookSpot/models.py
+++ cookproject/CookSpot/models.py
@@ -1,3 +1,38 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    name = models.CharField(max_length=254)
+    username = models.CharField(max_length=254)
+    birthdate = models.DateField()
+    email = models.CharField(max_length=254)
+    selfdesc = models.TextField()
+    def __str__(self):
+        return self.name
+    
+class Recipe(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=254)
+    properties = models.CharField(max_length=254)
+    instructions = models.TextField()
+    
+    def __str__(self):
+        return self.name
+
+class Ingredients(models.Model):
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
+    name = models.CharField(max_length=254)
+    amount = models.CharField(max_length=254)
+
+    def __str__(self):
+        return self.name
+
+class Meets(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
+    name = models.CharField(max_length=254)
+    date = models.DateField()
+    desc = models.TextField()
+
+    def __str__(self):
+        return self.name
