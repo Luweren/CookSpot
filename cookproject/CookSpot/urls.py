e38c8cd19fcde648fb4940a88483ef35157a994a
Luweren <luwerenmade@gmail.com>
2021-05-30 10:59:26 +0200

    edited html linking and urls

--- cookproject/CookSpot/urls.py
+++ cookproject/CookSpot/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns=[
-    
+    path("users/", views.users, name="users"),
+    path("", views.homepage, name="home"),
+    path("homepage/", views.homepage, name="home"),
 ]
--- cookproject/CookSpot/views.py
+++ cookproject/CookSpot/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse, HttpResponseRedirect
+from django.template import loader
+from django.urls import reverse
 # Create your views here.
+def homepage(response):
+    return render(response, "homepage.html",{})
+def users(response):
+    return render(response, "users.html",{})
