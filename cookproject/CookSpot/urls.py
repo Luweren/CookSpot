cdeae7661665a422f5179e911b58a9ed62e7e7a8
Niclask <niclas.koenig@campus.tu-berlin.de>
2021-05-31 19:55:48 +0200

    implemented dynamic url for recipes

--- cookproject/CookSpot/urls.py
+++ cookproject/CookSpot/urls.py
@@ -5,4 +5,7 @@ urlpatterns=[
     path("users/", views.users, name="users"),
     path("", views.homepage, name="home"),
     path("homepage/", views.homepage, name="home"),
+    path("recipe/", views.recipe, name="recipe"),
+    path('<str:User_username>/<str:Recipe_name>/', views.recipe, name='recipe'),
+    path('<User_name>', views.recipe, name='recipe2'),
 ]
--- cookproject/CookSpot/views.py
+++ cookproject/CookSpot/views.py
@@ -2,8 +2,15 @@ from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
 from django.template import loader
 from django.urls import reverse
+from .models import User, Recipe, Ingredients, Meets
 # Create your views here.
 def homepage(response):
     return render(response, "homepage.html",{})
 def users(response):
     return render(response, "users.html",{})
+def recipe(response,User_username,Recipe_name):
+    user = User.objects.get(username=User_username)
+    recipe = user.recipe_set.get(name=Recipe_name)
+    if(user != 0):
+        if(recipe != 0):
+            return render(response, "recipe.html",{'user':user, 'recipe':recipe})
